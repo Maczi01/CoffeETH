48505892b8be7bc78f40d7e0e1c9ac2d374ca0a4
MateuszTwardy <mateusz.w.twardy@gmail.com>
2022-10-29 21:47:01 +0100

    Initial

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.9;
 
 contract BuyMeACoffee {
-    //    Event emit when a Memo is created
-    event NewMemo {
-address indexed from;
-uint256 timestamp;
-string name;
-string message;
-}
+    // Event to emit when a Memo is created.
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
 
-    //    Memo struct
+    // Memo struct.
     struct Memo {
         address from;
         uint256 timestamp;
@@ -18,4 +18,52 @@ string message;
         string message;
     }
 
+    //address of contract deployer
+    address payable owner;
+
+    //    Array of Memos
+    Memo[] memos;
+
+    //    Constructor
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    ///*
+    // @dev buy a coffee for contract getownpropertydescriptors
+    // @param _name name of the coffee buyer
+    // @param _message a some nice message to coffee buyer
+
+    function buyCoffee(string memory _name, string memory _message) public payable {
+        require(msg.value > 0, "Can't buy coffee with 0ETH!");
+        //        add memo to memos array(storage)
+        memo.push(Memo(
+                msg.sender,
+                block.timestamp,
+                _name,
+                _message
+            ));
+
+//        Emit a log when a new memo is created
+        emit NewMemo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        );
+    }
+
+//**
+//*   @dev withdraw all the contract balance to owner
+//*/
+    function withdrawTips() public {
+        require(owner.send(address(this).balance));
+    }
+
+    //**
+    // @dev get all memos received and stored in blockchain
+    //*/
+    function getMemos() public view returns (Memo[] memory) {
+        return memos;
+    }
 }
